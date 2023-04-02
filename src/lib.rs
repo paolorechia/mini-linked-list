09977d56673349f6cbec298b37ca770cd12f46be
Paolo Rechia <paolorechia@gmail.com>
2023-04-02 21:26:11 +0200

    Implements pop left

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,10 @@ pub struct LinkedList<T> {
     pub val: Option<T>,
     pub next: Option<Box<LinkedList<T>>>,
 }
+pub struct PopLeftResult<T> {
+    pub val: Option<T>,
+    pub list: Option<Box<LinkedList<T>>>
+}
 
 impl LinkedList<i32> {
     pub fn new() -> LinkedList<i32>{
@@ -33,20 +37,30 @@ impl LinkedList<i32> {
         node
     }
 
+    pub fn pop_left(self) -> PopLeftResult<i32> {
+        if self.val.is_some() {
+            return PopLeftResult { val: self.val, list: self.next }
+        }
+        match self.next {
+            Some(node) => PopLeftResult { val: node.val, list: node.next },
+            None => PopLeftResult { val: self.val, list: None }
+        }
+    }
 
     pub fn collect(&self) -> Vec<i32> {
         let mut result = Vec::<i32>::new();
         let mut node = self;
+
+        match node.val {
+            Some(val) => result.push(val),
+            _ => ()
+        }
         while node.next.is_some() {
+            node = node.next.as_ref().unwrap();
             match node.val {
                 Some(val) => result.push(val),
                 _ => ()
             }
-            node = node.next.as_ref().unwrap();
-        }
-        match node.val {
-            Some(val) => result.push(val),
-            _ => ()
         }
 
         return result;
@@ -75,5 +89,37 @@ mod tests {
         list = list.push_left(3);
         list = list.push_left(4);
         assert_eq!(list.collect(), vec![4,3,2,1]);
+    }
+    
+    #[test]
+    fn test_linked_list_pop_left() {
+        let mut list: LinkedList<i32> = LinkedList::<i32>::new();
+        list.push_right(1);
+        list.push_right(2);
+        list.push_right(3);
+        list.push_right(4);
+
+        let result = list.pop_left();
+        let list = result.list.unwrap();
+
+        assert_eq!(list.collect(), vec![2,3,4]);
+        assert_eq!(result.val.unwrap(), 1);
+
+        let result = list.pop_left();
+        let list = result.list.unwrap();
+
+        assert_eq!(list.collect(), vec![3,4]);
+        assert_eq!(result.val.unwrap(), 2);
+
+        let result = list.pop_left();
+        let list = result.list.unwrap();
+
+        assert_eq!(list.collect(), vec![4]);
+        assert_eq!(result.val.unwrap(), 3);
+
+        let result = list.pop_left();
+        assert_eq!(result.list.is_none(), true);
+        assert_eq!(result.val.unwrap(), 4);
+
     }
 }
